7e2a24dad1de7673225834462fadcc48e5b082f6
Prakash-kumar1 <kumarsanju4881@gmail.com>
2022-06-22 13:39:40 +0530

    Function pass into multiple variable

---
+++ REVISION/facto.js
@@ -0,0 +1,19 @@
+// Factorial function passing into Multiple variable
+
+function facto(num){
+    let fact = 1 ;
+    for(let i=num; i>0; i--){           
+        fact*= i ;
+    }
+    return fact
+}                                 
+//  Function are Ist class citizens it means we can pass into variable
+
+   let b = facto ;
+   console.log(b(5))
+
+   let c = facto ;
+   console.log(c(4))
+
+   let d = facto ;
+   console.log(d(3))
