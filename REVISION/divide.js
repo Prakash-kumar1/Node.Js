45dc480ef1c9f402ff621ba33074acb96f4e4702
Prakash-kumar1 <kumarsanju4881@gmail.com>
2022-06-22 12:28:12 +0530

    Arrow function for division

---
+++ REVISION/divide.js
@@ -0,0 +1,9 @@
+// making Arrow function for division
+
+let divide = (a,b) => {
+    let c = Math.floor(a/b)
+    return c;
+}
+console.log(divide(10,2))
+console.log(divide(10,3))
+console.log(divide(100,2))
