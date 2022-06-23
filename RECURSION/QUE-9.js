73ad209e6bb5ea33605cc0ec49aa50dbd4da1a17
Prakash-kumar1 <kumarsanju4881@gmail.com>
2022-06-23 23:26:54 +0530

    printing odd no. using recursion

---
+++ RECURSION/QUE-9.js
@@ -0,0 +1,11 @@
+// Question-9 : Given a number n, print all the odd numbers from 1 to n using recursion.
+
+function odd(n,i){
+    if(i<=n){
+        if(i%2!=0){
+            console.log(i)
+        }
+        return odd(n,i+1)
+    }
+}
+console.log(odd(20,1))
