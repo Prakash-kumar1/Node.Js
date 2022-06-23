0f2b646f606751839ba5682c06b2358ccf55467d
Prakash-kumar1 <kumarsanju4881@gmail.com>
2022-06-23 19:49:31 +0530

    Fibbonaci series using Recursion

--- NESTEDFUNCTION/fibbonaci.js
+++ NESTEDFUNCTION/fibbonaci.js
@@ -1,5 +1,7 @@
+// print 10th digit in Fibbonaci Series
+
 function fibo(first , second, counter){
-    if(counter<=10){
+    if(counter<10){
     let c = first + second ;
     return fibo( second, c, counter+1) ;
     }
