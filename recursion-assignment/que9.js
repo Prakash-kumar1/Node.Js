9de003d99ab716a38d7d4df4d4a6a512e2a2c4c8
Prakash-kumar1 <kumarsanju4881@gmail.com>
2022-06-25 23:03:34 +0530

    sum of all the first 10 even numbers.

---
+++ recursion-assignment/que9.js
@@ -0,0 +1,14 @@
+// Question No.-9: Print sum of all the first 10 even numbers.
+
+function sum(a,i,s){
+    if(i<=10){
+        if(a%2==0){
+            s=s+a
+            return sum(a+1,i+1,s)
+        }
+        return sum(a+1,i,s)
+    }
+    console.log(s)
+}
+
+console.log(sum(1,1,0))
