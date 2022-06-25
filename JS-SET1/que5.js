5baa84672c0e6dc084e09981833b878388f6112b
Prakash-kumar1 <kumarsanju4881@gmail.com>
2022-06-25 15:27:15 +0530

    Product of two numbers

--- JS-SET1/que5.js
+++ JS-SET1/que5.js
@@ -1,9 +1,14 @@
+// Question No.-5: Take two numbers and find product of all numbers between them that satisfy following condition (inclusive range): 
+// Condition 1 - Second last digit of number is 4
+// Condition 2 - Numbers should be even 
+
+
 let a = parseInt(process.argv[2])
 let b = parseInt(process.argv[3])
 let p = 1
 for(let i=a;i<=b;i++){
-  let check=Math.floor(i/10)
-  if(i%2==0 && check%10==4){
+  let Second_last=Math.floor(i/10)
+  if(i%2==0 && Second_last%10==4){
     p=p*i
   }
 }  
