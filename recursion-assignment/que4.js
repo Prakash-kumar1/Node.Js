0f37d4680ae70097eccc14b1395c3e331319aa3d
Prakash-kumar1 <kumarsanju4881@gmail.com>
2022-06-25 22:53:01 +0530

    Factorial using Recursion

---
+++ recursion-assignment/que4.js
@@ -0,0 +1,11 @@
+// Question No.-4: Find the factorial of a given number n.
+
+function fact(no,p){
+    if(no>0){
+        p=p*no
+        return fact(no-1,p)
+    }
+    console.log(p)
+}
+
+console.log(fact(5,1))
