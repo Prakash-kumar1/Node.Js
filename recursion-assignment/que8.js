741ec47efea084739766a3f2d6c2444f6f99add3
Prakash-kumar1 <kumarsanju4881@gmail.com>
2022-06-25 23:01:53 +0530

    Checking prime no. using Recursion

---
+++ recursion-assignment/que8.js
@@ -0,0 +1,17 @@
+// Question No.-8:  Write the recursive function to check whether a given number is prime or not.
+
+function prime(no,i){
+
+    if(i<no){
+        if(no%i==0){
+            return no+" is Not a Prime Number"
+        }
+        else {
+            return prime(no,i+1)
+        }
+    }
+    return (no+" is A Prime Number")
+}
+
+console.log(prime(23,2))
+console.log(prime(22,2))
