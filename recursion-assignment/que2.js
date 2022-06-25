b5935ac2bb9e3f2179f53d056e27dbc1dea3db1c
Prakash-kumar1 <kumarsanju4881@gmail.com>
2022-06-25 22:49:00 +0530

    Printing descending order using Recursion

---
+++ recursion-assignment/que2.js
@@ -0,0 +1,10 @@
+// Question No.-2: Print all the numbers in descending order in the given range
+
+function allNumber(a,b){
+    if(a<=b){
+        console.log(b)
+        return allNumber(a,b-1)
+    }
+}
+
+console.log(allNumber(10,20))
