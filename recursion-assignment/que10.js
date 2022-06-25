f408fd74dbf500f0b797bc0a5846aef511029008
Prakash-kumar1 <kumarsanju4881@gmail.com>
2022-06-25 23:05:57 +0530

    find the power Using Recursion

---
+++ recursion-assignment/que10.js
@@ -0,0 +1,12 @@
+// Question No.-10: Write a recursive program to find the power of given number and exponent.
+//  (without using Math.pow and exponent operator(**)).
+
+function power(no,exp,ans){
+    if(exp>0){
+        ans=ans*no
+        return (power(no,exp-1,ans))
+    }
+    console.log(ans)
+}
+
+console.log(power(2,4,1))
