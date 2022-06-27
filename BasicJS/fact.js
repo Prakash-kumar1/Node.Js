647c94e6b59c43fa3661dfaf6d986b5ee6b92449
Prakash-kumar1 <kumarsanju4881@gmail.com>
2022-06-27 13:58:23 +0530

    checking output on diff.  variables

--- BasicJS/fact.js
+++ BasicJS/fact.js
@@ -5,11 +5,15 @@ function facto(num){
     for(let i = num; i>0; i--){
         fact*=i;
     }
-      return fact 
+      return fact ;
 }
-let ans = facto(5);   
-let x = facto(4);
-let y = facto(3);
+let ans = facto(5) ;   
+let x = facto(4) ;
+let y = facto(3) ;
+ y = facto(0) ;
+ y = facto(-1) ;
 console.log(ans);
 console.log(x);
+console.log(y);
+console.log(y);
 console.log(y);
