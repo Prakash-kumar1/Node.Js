9e9f39205ba34dc9d1e986a4cafb46109fdc4e5b
Prakash-kumar1 <kumarsanju4881@gmail.com>
2022-06-28 22:55:03 +0530

    Rectangle Area using Function.

--- REVISION/rectangle.js
+++ REVISION/rectangle.js
@@ -3,5 +3,8 @@
 let ans = (function  (length,breadth){
     let area = length*breadth
     return area ;
-})(12,10)
-  console.log (ans)
+})(10,10)
+  console.log (ans) ;
+
+  // it will just print 10,10 just run on terminal.
+  console.log (10,10) ;
