0e387a2a277548b67ed16fcfd1573461f9514e84
Prakash-kumar1 <kumarsanju4881@gmail.com>
2022-06-28 22:47:56 +0530

    Practicing some basics of  Javascript.

---
+++ Keyword/base.js
@@ -0,0 +1,23 @@
+// console.log("fname");
+// console.log(prakash);
+// console.log(fname);
+// const fname = 'vasanth'
+// console.log(fname);
+
+const number = 2;
+// const can not be declared only intialized
+console.log(number);
+
+// var fname = "Prakash"
+//  fname = "kumar"
+// var fname = "singh"
+// console.log(fname);
+
+
+console.log(getname())
+function getname(){
+    // console.log(lname);
+    var lname = 'kumar'
+    console.log(lname);
+    console.log('fname');
+}
