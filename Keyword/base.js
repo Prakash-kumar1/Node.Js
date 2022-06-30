3edc900f88fb4b8e6dbe2a67bd76c4c7a9c6d168
Prakash-kumar1 <kumarsanju4881@gmail.com>
2022-06-30 23:34:32 +0530

    basic

--- Keyword/base.js
+++ Keyword/base.js
@@ -1,46 +1,48 @@
-console.log("fname");
-console.log(prakash);
-console.log(fname);
-const fname = 'vasanth'
-console.log(fname);
+// console.log("fname");
+// console.log(prakash);
+// console.log(fname);
+// const fname = 'vasanth'
+// console.log(fname);
 
-const number = 2;
-// const can not be declared only intialized
-console.log(number);
+// const number = 2;
+// // const can not be declared only intialized
+// console.log(number);
 
-var fname = "Prakash"
- fname = "kumar"
-var fname = "singh"
-console.log(fname);
-
-
-console.log(getname())
-function getname(){
-    // console.log(lname);
-    var lname = 'kumar'
-    console.log(lname);
-    console.log('fname');
-}
-
-for (let i = 0; i < 2;i++){
-    for(let j = 0; j < 2;j++){
-        for(let k = 0 sole.g(i,j,k);
-
-        }
-    }
-}
-let num = 3 ;
-for (let i = 1; i <= num; i++){
-   let stars = ' '
-   for (let j = 1; j < num; j++){
-    stars = stars + '*'
-    console.log(stars)
-}
-      console.log(stars)
-}
+// var fname = "Prakash"
+//  fname = "kumar"
+// var fname = "singh"
+// console.log(fname);
 
 
+// console.log(getname())
+// function getname(){
+//     // console.log(lname);
+//     var lname = 'kumar'
+//     console.log(lname);
+//     console.log('fname');
+// }
 
+// for (let i = 0; i < 2;i++){
+//     for(let j = 0; j < 2;j++){
+//         for(let k = 0 sole.g(i,j,k);
+
+//         }
+//     }
+// }
+// let num = 3 ;
+// for (let i = 1; i <= num; i++){
+//    let stars = ' '
+//    for (let j = 1; j < num; j++){
+//     stars = stars + '*'
+//     console.log(stars)
+// }
+//       console.log(stars)
+// }
+
+
+setTimeout(function num(){
+      console.log("hello world")
+},(2000))
 
 
 
