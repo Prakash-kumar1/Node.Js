1803401e730d0f48999b90bcffd779a17b7eeab9
Prakash-kumar1 <kumarsanju4881@gmail.com>
2022-06-30 21:07:51 +0530

    basic js

--- Keyword/base.js
+++ Keyword/base.js
@@ -29,15 +29,18 @@
 //         }
 //     }
 // }
-let num = 3 ;
-for (let i = 1; i <= num; i++){
-   let stars = ' '
-   for (let j = 1; j < num; j++){
-    stars = stars + '*'
-    console.log(stars)
-}
-      console.log(stars)
-}
+// let num = 3 ;
+// for (let i = 1; i <= num; i++){
+//    let stars = ' '
+//    for (let j = 1; j < num; j++){
+//     stars = stars + '*'
+//     console.log(stars)
+// }
+//       console.log(stars)
+// }
+
+
+
 
 
 
