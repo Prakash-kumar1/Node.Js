06e0ad05be95fb2114ef4a35a9dd25307b5f7e3b
Prakash-kumar1 <kumarsanju4881@gmail.com>
2022-07-01 11:36:40 +0530

    Fibbonaci series in Pattern

---
+++ Pattern-Assignment/QNo-4.js
@@ -0,0 +1,19 @@
+let a=0;
+let b=1;
+let i=1;
+let x=5
+
+for(j=1;j<=x;j++){
+    let str=""
+for(k=1;k<=j;k++){
+    while(i<=((j*(j+1)/2))){
+        // console.log(a)
+        str=str+`${a} `
+        c=a+b
+        a=b
+        b=c
+        i++
+    } 
+}
+console.log(str)
+}
