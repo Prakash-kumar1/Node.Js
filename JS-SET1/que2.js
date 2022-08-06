f15238df7107b06cb20c080706991ef83f046b56
Prakash-kumar1 <kumarsanju4881@gmail.com>
2022-08-06 20:33:15 +0530

    Print the sum of all even numbers in a range from a to b. (Including a,b)

--- JS-SET1/que2.js
+++ JS-SET1/que2.js
@@ -1,9 +1,9 @@
 // Question No.-2: Print the sum of all even numbers in a range from a to b. (Including a,b)
 
-sum=0
-a=parseInt(process.argv[2])
-b=parseInt(process.argv[3])
+sum=0 ;
+a=parseInt(process.argv[2]) ;
+b=parseInt(process.argv[3]) ;
 for(let i=a; i<=b; i++){
-    sum=sum+i
+    sum=sum+i ;
 }
 console.log(sum)
