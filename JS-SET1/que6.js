b7f461dc44b4c18c03a6a84846548864aac36ab0
Prakash-kumar1 <kumarsanju4881@gmail.com>
2022-08-06 20:36:06 +0530

    Take a number and return all the even digits of the number.

--- JS-SET1/que6.js
+++ JS-SET1/que6.js
@@ -1,11 +1,11 @@
 // Question No.-6: Take a number and return all the even digits of the number.
 
-n=parseInt(process.argv[2])
-let i=1
+n=parseInt(process.argv[2]) ;
+let i=1 ;
 while(n!=0){
-    rem=n%10
-    n=Math.floor(n/10)
-    i++
+    rem=n%10 ;
+    n=Math.floor(n/10) ;
+    i++ ;
     if(i%2==0){
         console.log(rem);
     }   
