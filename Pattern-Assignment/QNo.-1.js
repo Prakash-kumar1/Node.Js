d3e07617798fc2c49f2400df5a396d8a4ba082f5
Prakash-kumar1 <kumarsanju4881@gmail.com>
2022-08-06 20:37:19 +0530

    Printing Patteren

--- Pattern-Assignment/QNo.-1.js
+++ Pattern-Assignment/QNo.-1.js
@@ -1,6 +1,6 @@
  let n=11;
  for(i=1;i<=n;i++){
-  let str=''
+  let str='' ;
   for(j=1;j<=n;j++){
     if(i==j || (i+j==n+1)){
         str=str+`${' * '}`
@@ -9,5 +9,5 @@
             str=str+`${'   '}`
                  }
   }
-  console.log(str)
+  console.log(str) ;
  }
