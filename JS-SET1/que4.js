a1037d807cff944c109dda4602a5fa0c32dd8bb9
Prakash-kumar1 <kumarsanju4881@gmail.com>
2022-08-06 20:34:43 +0530

    Check whether the given number is an armstrong number or not.

--- JS-SET1/que4.js
+++ JS-SET1/que4.js
@@ -1,20 +1,20 @@
 // Question No.-4: Check whether the given number is an armstrong number or not. 
 
-size=parseInt(process.argv[2])
-number=parseInt(process.argv[3])
-num=number
-let armstrong_num=0
-let digit=0
-let i=1
+size=parseInt(process.argv[2]);
+number=parseInt(process.argv[3]) ;
+num=number ;
+let armstrong_num = 0 ;
+let digit=0 ;
+let i=1 ;
 while(i<=size){
-    digit=number%10
-    armstrong_num=armstrong_num+(digit**size)
-    number=Math.floor(number/10)
-    i++
+    digit=number%10 ;
+    armstrong_num=armstrong_num+(digit**size) ;
+    number=Math.floor(number/10) ;
+    i++ ;
 }
 if(armstrong_num==num){
-    console.log("yes")
+    console.log("yes") ;
 }
 else{
-    console.log("no");
+    console.log("no"); 
 }
