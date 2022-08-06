ffbac5e8252261c768ca07e244e4b3ed996e6065
Prakash-kumar1 <kumarsanju4881@gmail.com>
2022-08-06 20:35:32 +0530

    find product of all numbers between them

--- JS-SET1/que5.js
+++ JS-SET1/que5.js
@@ -3,16 +3,16 @@
 // Condition 2 - Numbers should be even 
 
 
-let a = parseInt(process.argv[2])
-let b = parseInt(process.argv[3])
-let p = 1
+let a = parseInt(process.argv[2]) ;
+let b = parseInt(process.argv[3]) ;
+let p = 1 ;
 for(let i=a;i<=b;i++){
-  let Second_last=Math.floor(i/10)
+  let Second_last=Math.floor(i/10) ;
   if(i%2==0 && Second_last%10==4){
-    p=p*i
+    p=p*i ;
   }
 }  
 if(p==0){
-  console.log(0)
+  console.log(0) ;
 }
- console.log(p)
+ console.log(p) ;
