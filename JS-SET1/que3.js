5d0536e475a54d521a9caed91cd2c6bbd908c38f
Prakash-kumar1 <kumarsanju4881@gmail.com>
2022-08-06 20:33:51 +0530

    Write a program to read the first and last digit of a number

--- JS-SET1/que3.js
+++ JS-SET1/que3.js
@@ -1,7 +1,7 @@
 // Question No.-3: Write a program to read the first and last digit of a number
 
-n=parseInt(process.argv[2])
-first_digit=Math.floor(n/10)
-last_digit=n%10
-console.log("firstdigit:"+first_digit)
-console.log("lastdigit:"+last_digit)
+n = parseInt(process.argv[2]) ;
+first_digit=Math.floor(n/10) ;
+last_digit=n%10 ;
+console.log("firstdigit:"+first_digit) ;
+console.log("lastdigit:"+last_digit) ;
