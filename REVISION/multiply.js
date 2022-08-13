8b5dda65690704bce7ad1c511aa4aafff86035d3
Prakash-kumar1 <kumarsanju4881@gmail.com>
2022-08-13 22:23:49 +0530

    Printing Multiplication of a Number using Function

--- REVISION/multiply.js
+++ REVISION/multiply.js
@@ -2,6 +2,6 @@ function cross(p,q){
     let r = p*q ;
      return r ;
 }
-console.log(cross(2,4))
-console.log(cross(2,-4))
-console.log(cross(-5,-4))
+console.log(cross(2,4)) ;
+console.log(cross(2,-4)) ;
+console.log(cross(-5,-4)) ;
