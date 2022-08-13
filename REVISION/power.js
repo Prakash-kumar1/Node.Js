ec08e36ad75f6bd30173b9c0b7bfca55d8b16920
Prakash-kumar1 <kumarsanju4881@gmail.com>
2022-08-13 22:22:57 +0530

    Power of a Number using IIFE Function

--- REVISION/power.js
+++ REVISION/power.js
@@ -1,7 +1,8 @@
 // making Power of a Number using IIFE Function
 
 let ans = (function Power(a,b){
-    let c = a**b
+    let c = a**b ;
     return c ;
-})(4,2)
-console.log(ans)
+})(4,2) 
+
+console.log(ans) ;
