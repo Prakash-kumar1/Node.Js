a74f0c8a769d76fd9f01ce2768f7471e7f16f505
Prakash-kumar1 <kumarsanju4881@gmail.com>
2022-08-13 22:22:26 +0530

    finding Area of Rectangle using Anoynomous IIFE Function

--- REVISION/rectangle.js
+++ REVISION/rectangle.js
@@ -1,9 +1,10 @@
 // finding Area of Rectangle using Anoynomous IIFE Function
 
 let ans = (function  (length,breadth){
-    let area = length*breadth
+    let area = length*breadth ;
     return area ;
 })(10,10)
+
   console.log (ans) ;
 
   // it will just print 10,10 just run on terminal.
